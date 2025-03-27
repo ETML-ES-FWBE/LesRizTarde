9bb7207b618956a04656baf4c2fbaf829078d58a
Smartezz <pm17nrr@eduvaud.ch>
2025-03-27 17:14:21 +0100

    feat: Ajout student repository

---
+++ src/main/java/ch/etmles/payroll/Repositories/StudentRepository.java
@@ -0,0 +1,9 @@
+package ch.etmles.payroll.Repositories;
+
+import ch.etmles.payroll.Entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
